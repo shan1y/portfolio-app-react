942c41d17a066865447462a70b429dbdb963edeb
Shannon Yazdani <shannonyazdani@gmail.com>
2022-07-28 14:31:01 -0400

    add hover effects to projects

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import WorkImg from "../assets/workImg.jpeg";
-import realEstate from "../assets/realestate.jpg";
 import coffee from "../assets/projectImages/coffee.png";
 import bandsite from "../assets/projectImages/bandsite.png";
 import travelsite from "../assets/projectImages/travelsite.png";
@@ -24,142 +22,160 @@ const Work = () => {
           {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${coffee})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div-image"
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
+            <div class="content-div h-full w-full flex flex-col justify-center">
+            <div className="opacity-0 group-hover:opacity-100 text-center flex flex-col">
+              <span className="text-2xl font-bold text-white tracking-wider text-center">
                 Coffee Shop
               </span>
               <div className="pt-8 text-center">
-                <a href="https://coffee-shop-bstn.netlify.app/index.html">
+                <a href="https://coffee-shop-bstn.netlify.app/index.html" rel="noopener noreferrer" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
                 <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  {/* <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
-                  </button>
+                  </button> */}
                 </a>
               </div>
+              <p className="text-center mt-4">HTML | CSS | JS | React | Express</p>
+            </div>
             </div>
           </div>
           <div
             style={{ backgroundImage: `url(${travelsite})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div-image"
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
+            <div class="content-div h-full w-full flex flex-col justify-center">
+            <div className="opacity-0 group-hover:opacity-100 flex flex-col">
+              <span className="text-2xl font-bold text-white tracking-wider text-center ">
                 Travel Site
               </span>
               <div className="pt-8 text-center">
-                <a href="https://travel-site-bstn.netlify.app/">
+                <a href="https://travel-site-bstn.netlify.app/" rel="noopener noreferrer" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
                 <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                  {/* <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
-                  </button>
+                  </button> */}
                 </a>
               </div>
+              <p className="text-center mt-4">HTML | CSS | JS | React | Express</p>
+            </div>
             </div>
           </div>
           {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${bandsite})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div-image"
           >
             {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
+            <div class="content-div h-full w-full flex flex-col justify-center">
+            <div className="opacity-0 group-hover:opacity-100 flex flex-col">
+              <span className="text-2xl font-bold text-white tracking-wider text-center">
                 Bandsite
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://bandsite-project.netlify.app/" rel="noopener noreferrer" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="https://brainflix-bstn.netlify.app/">
+                {/* <a href="https://bandsite-project.netlify.app/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
-                </a>
+                </a> */}
               </div>
+              <p className="text-center mt-4">HTML | CSS | JS | React | Express</p>
+            </div>
             </div>
           </div>
           <div
             style={{ backgroundImage: `url(${brainflix})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div-image"
           >
             {/* Hover Effects */}
+            <div class="content-div h-full w-full flex flex-col justify-center">
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
+              <span className="text-2xl font-bold text-white tracking-wider text-center flex flex-col">
                 BrainFlix
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://brainflix-bstn.netlify.app/" rel="noopener noreferrer" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                {/* <a href="/https://brainflix-bstn.netlify.app/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
-                </a>
+                </a> */}
               </div>
+              <p className="text-center mt-4">HTML | CSS | JS | React | Express</p>
+            </div>
             </div>
           </div>
           {/* Grid Item */}
           <div
             style={{ backgroundImage: `url(${instock})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div-image"
           >
             {/* Hover Effects */}
+            <div class="content-div h-full w-full flex flex-col justify-center">
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
+              <span className="text-2xl font-bold text-white tracking-wider text-center flex flex-col">
                 InStock Group Project
               </span>
               <div className="pt-8 text-center">
-                <a href="https://instock-project.herokuapp.com/inventory">
+                <a href="https://instock-project.herokuapp.com/inventory" rel="noopener noreferrer" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                {/* <a href="/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
-                </a>
+                </a> */}
               </div>
+              <p className="text-center mt-4">HTML | CSS | JS | React | Express</p>
+            </div>
             </div>
           </div>
           <div
             style={{ backgroundImage: `url(${skyball})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div-image"
           >
             {/* Hover Effects */}
+            <div class="content-div h-full w-full flex flex-col justify-center">
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">
+              <span className="text-2xl font-bold text-white tracking-wider text-center flex flex-col">
                 SkyBall Captsone
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://skyball.netlify.app" rel="noopener noreferrer" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                {/* <a href="/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
-                </a>
+                </a> */}
               </div>
+              <p className="text-center mt-4">HTML | CSS | JS | React | Express</p>
+            </div>
             </div>
           </div>
         </div>
