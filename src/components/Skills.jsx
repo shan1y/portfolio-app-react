ad619fd4addb917b98103fe97aa9506af44f3e59
Shannon Yazdani <shannonyazdani@gmail.com>
2022-08-04 13:22:05 -0400

    installed react router

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import HTML from "../assets/html.png";
 import CSS from "../assets/css.png";
 import JavaScript from "../assets/javascript.png";
@@ -10,7 +9,6 @@ import FireBase from "../assets/firebase.png";
 import MySQL from "../assets/mysql.png";
 import GitHub from "../assets/github.png";
 import Tailwind from "../assets/tailwind.png";
-import Mongo from "../assets/mongo.png";
 
 const Skills = () => {
   return (
