0ed197ee330513046125b3cc934dd4f3c7f140c4
Shannon Yazdani <shannonyazdani@gmail.com>
2022-08-13 07:02:27 -0400

    added z-index to nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
+    <div className="fixed z-10 w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
         <img
           src={Logo}
