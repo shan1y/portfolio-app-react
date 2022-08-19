9e3949f919fc4cb6f09d09d4b79067d2342eaf42
Shannon Yazdani <shannonyazdani@gmail.com>
2022-08-19 13:10:55 -0400

    added social icons to mobile

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { useState, useEffect } from "react";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import resume from "../assets/Resume/shannon-yazdani-resume.pdf";
 AOS.init();
 
 const Contact = () => {
@@ -34,8 +38,8 @@ const Contact = () => {
     }
     if (!values.email) {
       errors.email = "please enter your email";
-    } else if(!regex.test(values.email)){
-      errors.email = "this is not a valid email format"
+    } else if (!regex.test(values.email)) {
+      errors.email = "this is not a valid email format";
     }
     if (!values.message) {
       errors.message = "please enter a message";
@@ -44,61 +48,94 @@ const Contact = () => {
   };
 
   return (
-    <div
-      name="contact"
-      className="w-full h-screen md:h-screen bg-[#0a192f] flex justify-center items-center p-4"
-    >
-      <form
-        onSubmit={handleSubmit}
-        data-aos-duration="1000"
-        data-aos="fade-up"
-        method="POST"
-        action="https://getform.io/f/5b7a8336-8523-4dd7-afd2-123ddb0263d5"
-        className="flex flex-col max-w-[600px] w-full"
+    <>
+      <div
+        name="contact"
+        className="w-full h-screen md:h-screen bg-[#0a192f] flex flex-col justify-center items-center p-4"
       >
-        <div className="pb-8 mt-4  md:mt-0">
-          <p className=" text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
-            Contact
-          </p>
-          <p className="text-gray-300 pt-4 pb-0">
-            If you're looking for someone who’s highly motivated and who enjoys
-            being pushed creatively, let's chat! Submit the form below or shoot
-            me an e-mail - shannonyazdani at gmail.com
-          </p>
-        </div>
-        <input
-          className="bg-[#ccd6f6] p-2"
-          type="text"
-          placeholder="Name"
-          name="name"
-          value={formValues.name}
-          onChange={handleChange}
-        />
-         <p className="text-error mb-2 md:mb-4">{formErrors.name}</p>
-        <input
-          className=" p-2 bg-[#ccd6f6]"
-          // type="email"
-          placeholder="Email"
-          name="email"
-          value={formValues.email}
-          onChange={handleChange}
-        />
+        <form
+          onSubmit={handleSubmit}
+          data-aos-duration="1000"
+          data-aos="fade-up"
+          method="POST"
+          action="https://getform.io/f/5b7a8336-8523-4dd7-afd2-123ddb0263d5"
+          className="flex flex-col max-w-[600px] w-full"
+        >
+          <div className="pb-8 mt-4  md:mt-0">
+            <p className=" text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
+              Contact
+            </p>
+            <p className="text-gray-300 pt-4 pb-0">
+              If you're looking for someone who’s highly motivated and who
+              enjoys being pushed creatively, let's chat! Submit the form below
+              or shoot me an e-mail - shannonyazdani at gmail.com
+            </p>
+          </div>
+          <input
+            className="bg-[#ccd6f6] p-2"
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={formValues.name}
+            onChange={handleChange}
+          />
+          <p className="text-error mb-2 md:mb-4">{formErrors.name}</p>
+          <input
+            className=" p-2 bg-[#ccd6f6]"
+            // type="email"
+            placeholder="Email"
+            name="email"
+            value={formValues.email}
+            onChange={handleChange}
+          />
           <p className="text-error mb-2 md:mb-4">{formErrors.email}</p>
-        <textarea
-   
-          className="bg-[#ccd6f6] p-2 resize-none"
-          name="message"
-          rows="6"
-          placeholder="Message"
-          value={formValues.message}
-          onChange={handleChange}
-        ></textarea>
-         <p className="text-error">{formErrors.message}</p>
-        <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">
-          Let's Collaborate
-        </button>
-      </form>
-    </div>
+          <textarea
+            className="bg-[#ccd6f6] p-2 resize-none"
+            name="message"
+            rows="6"
+            placeholder="Message"
+            value={formValues.message}
+            onChange={handleChange}
+          ></textarea>
+          <p className="text-error">{formErrors.message}</p>
+          <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">
+            Let's Collaborate
+          </button>
+          <div className="lg:hidden flex text-white">
+            <a
+              href="https://www.linkedin.com/in/shannonyazdani/"
+              className="flex flex-col items-center w-full text-gray-300"
+            >  <FaLinkedinIn size={30} />
+              LinkedIn
+            
+            </a>
+            <a
+              href="https://github.com/shan1y"
+              className="flex flex-col items-center w-full text-gray-300"
+            >   <FaGithub size={30} />
+              {" "}
+              GitHub
+           
+            </a>
+            <button
+              onClick={() =>
+                (window.location = "mailto:shannonyazdani@gmail.com.com")
+              }
+              className="flex flex-col items-center w-full text-gray-300"
+            ><HiOutlineMail size={30} />
+              Email 
+            </button>
+            <a
+              className="flex flex-col items-center w-full text-gray-300"
+              href={resume}
+              download="shannon-yazdani-resume.pdf"
+            ><BsFillPersonLinesFill size={30} />
+              Resume 
+            </a>
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
