b1f3f9590ff43e32f64610a41be55b854adec0ad
Shannon Yazdani <shannonyazdani@gmail.com>
2022-09-04 12:59:46 -0400

    changed navbar height

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,10 @@
 import React, { useState } from "react";
-import {
-  FaBars,
-  FaTimes,
-  FaGithub,
-  FaLinkedin,
-} from "react-icons/fa";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/logo.png";
 import { Link } from "react-scroll";
-import resume from "../assets/Resume/shannon-yazdani-resume.pdf"
+import resume from "../assets/Resume/shannon-yazdani-resume.pdf";
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -19,38 +14,67 @@ const Navbar = () => {
     <div className="fixed z-10 w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
         <a href="/">
-        <img
-          src={Logo}
-          alt="Logo Image"
-          style={{ height:"80px", transform: "translateX(-25px) " }}
-        />
+          <img
+            src={Logo}
+            alt="Logo Image"
+            style={{ height: "65px", transform: "translateX(-25px) " }}
+          />
         </a>
       </div>
 
       {/* menu */}
       <ul className="hidden md:flex">
         <li>
-          <Link class="hover:text-[#DB2777]"to="home"  smooth={true} duration={500}>
+          <Link
+            class="hover:text-[#DB2777]"
+            to="home"
+            smooth={true}
+            duration={500}
+          >
             Home
           </Link>
         </li>
         <li>
-          <Link class="hover:text-[#DB2777]" to="about" offset={0} smooth={true} duration={500}>
+          <Link
+            class="hover:text-[#DB2777]"
+            to="about"
+            offset={0}
+            smooth={true}
+            duration={500}
+          >
             About
           </Link>
         </li>
         <li>
-          <Link class="hover:text-[#DB2777]" to="skills" offset={0} smooth={true} duration={500}>
+          <Link
+            class="hover:text-[#DB2777]"
+            to="skills"
+            offset={0}
+            smooth={true}
+            duration={500}
+          >
             Skills
           </Link>
         </li>
         <li>
-          <Link class="hover:text-[#DB2777]" to="work"  offset={0} smooth={true} duration={500}>
+          <Link
+            class="hover:text-[#DB2777]"
+            to="work"
+            offset={0}
+            smooth={true}
+            duration={500}
+          >
             Work
           </Link>
         </li>
         <li>
-          <Link  class="hover:text-[#DB2777]" to="contact" offset={0} smooth={true} duration={500}>
+          <Link
+            class="hover:text-[#DB2777]"
+            to="contact"
+            offset={0}
+            smooth={true}
+            duration={500}
+          >
             Contact
           </Link>
         </li>
@@ -70,31 +94,66 @@ const Navbar = () => {
         }
       >
         <li className="py-6 text-4xl">
-          <Link class="hover:text-[#DB2777]" onClick={handleClick} to="home" offset={-0} smooth={true} duration={500}>
+          <Link
+            class="hover:text-[#DB2777]"
+            onClick={handleClick}
+            to="home"
+            offset={-0}
+            smooth={true}
+            duration={500}
+          >
             Home
           </Link>
         </li>
         <li className="py-6 text-4xl">
           {" "}
-          <Link  class="hover:text-[#DB2777]" onClick={handleClick} to="about" offset={-80} smooth={true} duration={500}>
+          <Link
+            class="hover:text-[#DB2777]"
+            onClick={handleClick}
+            to="about"
+            offset={-80}
+            smooth={true}
+            duration={500}
+          >
             About
           </Link>
         </li>
         <li className="py-6 text-4xl">
           {" "}
-          <Link class="hover:text-[#DB2777]" onClick={handleClick} to="skills" offset={-80} smooth={true} duration={500}>
+          <Link
+            class="hover:text-[#DB2777]"
+            onClick={handleClick}
+            to="skills"
+            offset={-80}
+            smooth={true}
+            duration={500}
+          >
             Skills
           </Link>
         </li>
         <li className="py-6 text-4xl">
           {" "}
-          <Link class="hover:text-[#DB2777]" onClick={handleClick} to="work" offset={-70} smooth={true} duration={500}>
+          <Link
+            class="hover:text-[#DB2777]"
+            onClick={handleClick}
+            to="work"
+            offset={-70}
+            smooth={true}
+            duration={500}
+          >
             Projects
           </Link>
         </li>
         <li className="py-6 text-4xl">
           {" "}
-          <Link class="hover:text-[#DB2777]" onClick={handleClick} to="contact"  offset={-30} smooth={true} duration={500}>
+          <Link
+            class="hover:text-[#DB2777]"
+            onClick={handleClick}
+            to="contact"
+            offset={-30}
+            smooth={true}
+            duration={500}
+          >
             Contact
           </Link>
         </li>
@@ -105,8 +164,8 @@ const Navbar = () => {
         <ul>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a
-                rel="noopener noreferrer"
-                target="_blank"
+              rel="noopener noreferrer"
+              target="_blank"
               className="flex justify-between items-center w-full text-gray-300"
               href="https://www.linkedin.com/in/shannonyazdani/"
             >
@@ -115,8 +174,8 @@ const Navbar = () => {
           </li>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
             <a
-                rel="noopener noreferrer"
-                target="_blank"
+              rel="noopener noreferrer"
+              target="_blank"
               className="flex justify-between items-center w-full text-gray-300"
               href="https://github.com/shan1y"
             >
@@ -125,9 +184,10 @@ const Navbar = () => {
           </li>
           <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
             <button
-            onClick={() => window.location = 'mailto:shannonyazdani@gmail.com.com'}
+              onClick={() =>
+                (window.location = "mailto:shannonyazdani@gmail.com.com")
+              }
               className="flex justify-between items-center w-full text-gray-300"
-      
             >
               Email <HiOutlineMail size={30} />
             </button>
